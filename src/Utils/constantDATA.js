71299a63f88a5aafaa0c2734e9494506afa412a9
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-18 19:41:39 +0530

    BASE_URL - DATA Added

--- src/Utils/constantDATA.js
+++ src/Utils/constantDATA.js
@@ -3,3 +3,5 @@ export const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 export const ACCEPT_ENCODING = "gzip, deflate, br";
 
 export const ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
+
+export const BASE_URL = "https://aniwatch.to";
