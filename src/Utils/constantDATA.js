6895a12c0b90f48d7dc9e0da69b8edc4091c638a
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-18 19:38:49 +0530

    ACCEPT_ENCODING - DATA Added

--- src/Utils/constantDATA.js
+++ src/Utils/constantDATA.js
@@ -1,2 +1,4 @@
 export const USER_AGENT =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
+
+export const ACCEPT_ENCODING = "gzip, deflate, br";
