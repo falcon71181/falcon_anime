6cb9ee1ea56aaf9dd7357519a736f914562c6ce3
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-18 19:08:13 +0530

    Basic Expressjs Server Created

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log("gg");
+import express from "express";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+const SERVER = '0.0.0.0';
+
+app.listen(PORT, SERVER, () => {
+    console.log(`⚔️  API @ http://${SERVER}:${PORT} @ STARTED  ⚔️`);
+  });
