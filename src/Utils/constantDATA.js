02e3ba30dab2fd149fa8dacf156df4f941ee9c21
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-18 19:42:10 +0530

    AJAX_URL - DATA Added

--- src/Utils/constantDATA.js
+++ src/Utils/constantDATA.js
@@ -5,3 +5,4 @@ export const ACCEPT_ENCODING = "gzip, deflate, br";
 export const ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
 
 export const BASE_URL = "https://aniwatch.to";
+export const AJAX_URL = "https://aniwatch.to/ajax";
