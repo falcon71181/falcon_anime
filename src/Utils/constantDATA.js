756339a81d56b4eea510af6512f1cf642f46010b
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-18 19:40:43 +0530

    ACCEPT - DATA Added

--- src/Utils/constantDATA.js
+++ src/Utils/constantDATA.js
@@ -1,4 +1,5 @@
-export const USER_AGENT =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
+export const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
 
 export const ACCEPT_ENCODING = "gzip, deflate, br";
+
+export const ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
