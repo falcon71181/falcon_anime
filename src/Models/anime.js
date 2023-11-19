6a4a4feb78745bfcf81d3d89aff9619ac59bea25
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-19 09:38:26 +0530

    Servers List Added - anime.js

---
+++ src/Models/anime.js
@@ -0,0 +1,16 @@
+export let Servers
+
+;(function(Servers) {
+  Servers["VidStreaming"] = "vidstreaming"
+  Servers["MegaCloud"] = "megacloud"
+  Servers["StreamSB"] = "streamsb"
+  Servers["StreamTape"] = "streamtape"
+  Servers["VidCloud"] = "vidcloud"
+  Servers["AsianLoad"] = "asianload"
+  Servers["GogoCDN"] = "gogocdn"
+  Servers["MixDrop"] = "mixdrop"
+  Servers["UpCloud"] = "upcloud"
+  Servers["VizCloud"] = "vizcloud"
+  Servers["MyCloud"] = "mycloud"
+  Servers["Filemoon"] = "filemoon"
+})(Servers || (Servers = {}))
