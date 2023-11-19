81484a3b824e9805df0b58599d49ad15a3c0ed0d
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-19 10:27:12 +0530

    Created async getCategory()

--- src/Controllers/CategoryC.js
+++ src/Controllers/CategoryC.js
@@ -2,3 +2,12 @@ import createHttpError from "http-errors";
 import RequestHandler from "express";
 import scrapeCategory from "../Parsers/Category";
 
+const getCategory = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default getCategory;
