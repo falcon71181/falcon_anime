68d2a3b8b04c6f4f6e93311ec219c617947234a1
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-19 10:31:34 +0530

    testing Basic req.json(data)

--- src/Controllers/CategoryC.js
+++ src/Controllers/CategoryC.js
@@ -4,7 +4,7 @@ import scrapeCategory from "../Parsers/Category";
 
 const getCategory = async (req, res) => {
     try {
-        
+        res.status(200).json(data);
     } catch (err) {
         console.log(err);
     }
