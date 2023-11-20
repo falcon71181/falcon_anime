5643f3b9ff7bafceeb0217604f81f385ae9c3261
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-20 09:55:18 +0530

    looping through DOM elements using each

--- src/Utils/extractANIME.js
+++ src/Utils/extractANIME.js
@@ -1,9 +1,14 @@
 import { CheerioAPI, SelectorType } from "cheerio";
 import createHttpError, { HttpError } from "http-errors";
 
-export const extractAnimes = ($, selector) => {
+export const extractAnimes = ($, selectors) => {
     try {
-        
+        const animes = [];
+        $(selectors).each((index,element) => {
+
+            
+        })
+        return animes;
     } catch (error) {
         
     }
