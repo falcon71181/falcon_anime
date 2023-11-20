bf157cb0135d2582e5f8383a24faae7e8c8a2658
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-20 09:19:10 +0530

    extractAnimes() Created

--- src/Utils/extractANIME.js
+++ src/Utils/extractANIME.js
@@ -1,3 +1,10 @@
 import { CheerioAPI, SelectorType } from "cheerio";
 import createHttpError, { HttpError } from "http-errors";
 
+export const extractAnimes = ($, selector) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
