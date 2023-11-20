de6f877c44ebeb74b147a6e3c1b7cde75c4b1f6b
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-21 00:07:59 +0530

    Creating async scrapeHome()

---
+++ src/Parsers/Home.js
@@ -0,0 +1,33 @@
+import createHttpError, { HttpError } from "http-errors";
+import axios, { AxiosError } from "axios";
+import { load } from "cheerio";
+import {
+    USER_AGENT_HEADER,
+    ACCEPT_ENCODING_HEADER,
+    URLs,
+    ACCEPT_HEADER
+} from '../Utils/constantDATA.js';
+import {
+  extractAnimes,
+  extractTOP10ANIMES
+} from '../Utils/extractANIME.js';
+
+async function scrapeHome() {
+    
+    try {
+        
+    } catch (err) {
+
+        ////////////////////////////////////////////////////////////////
+        console.error("Error in scrapeCategory:", err); // for TESTING//
+        ////////////////////////////////////////////////////////////////
+  
+          if (err instanceof AxiosError) {
+              throw createHttpError(
+                err?.response?.status || 500,
+                err?.response?.statusText || "Something went wrong"
+              )
+            }
+            throw createHttpError.InternalServerError(err?.message);
+    }
+}
