5784d351bbd9e5d0292c0b49a57915cc53a64182
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-21 00:08:52 +0530

    Scrapping trendingAnime Data

--- src/Parsers/Home.js
+++ src/Parsers/Home.js
@@ -15,7 +15,26 @@ import {
 async function scrapeHome() {
     
     try {
-        
+        const mainPage = await axios.get(URLs.HOME, {
+            headers: {
+              "User-Agent": USER_AGENT_HEADER,
+              "Accept-Encoding": ACCEPT_ENCODING_HEADER,
+              Accept: ACCEPT_HEADER
+            }
+        });
+        const $ = load(mainPage.data);
+        const trendingAnimeSelectors = "#anime-trending #trending-home .swiper-wrapper .swiper-slide";
+
+        $(trendingAnimeSelectors).each((index, element) => {
+            const animeID = $(element).find(".item .film-poster")?.attr("href")?.slice(1) || null;
+            const animeNAME = $(element).find(".item .number .film-title.dynamic-name")?.text()?.trim() ?? "UNKNOWN ANIME";
+            
+            res.animes.push({
+                id: animeID,
+                name: animeNAME,
+            })
+        })
+        return res;
     } catch (err) {
 
         ////////////////////////////////////////////////////////////////
