ab8bf08cb156a0f7e0aebd972573d8d61a2b5d5b
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-21 18:04:28 +0530

    making API and Aniwatch Health Check

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import router from "./Routes/router.js";
+import { URLs } from "./Utils/constantDATA.js";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 const SERVER = '0.0.0.0';
 
-app.get("/test", (req, res) => {
-  res.send("Worked");
+async function isSiteReachable(url) {
+  try {
+    const response = await fetch(url, { method: 'HEAD' });
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+}
+const isAniwatchUP = await isSiteReachable(URLs.BASE);
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    API: true,
+    aniwatch: true === isAniwatchUP
+  });
 });
 
 app.use("/anime", router);
