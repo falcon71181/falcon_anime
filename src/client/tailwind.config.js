407bbc558ffb033e4e6bf612fe574272d3955cab
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-22 11:15:16 +0530

    Extra Styling Added

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -1,13 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      animation: {
+        'rotate': 'spin 80s linear infinite',
+        'rotate-reverse': 'spin 80s linear infinite reverse',
+      },
+      keyframes: {
+        spin: {
+          from: {
+            transform: 'rotate(0deg)',
+          },
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
       colors: {
+        'profile_name': '#C1E47C',
         'saffron': '#fa4103',
         'grny': '#8cff00',
       },
@@ -21,6 +40,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+  },
+  variants: {
+    extend: {
+      transform: ['active'],
     },
   },
   plugins: [],
