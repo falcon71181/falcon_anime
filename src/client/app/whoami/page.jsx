f5a3dd949c7c4eba6afe9108aba184680af01893
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-25 20:31:15 +0530

    TextScramble() Added in whoami

--- src/client/app/whoami/page.jsx
+++ src/client/app/whoami/page.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from 'react';
 import ProfileHero from "@/components/ProfileHero";
 import ProfileContacts from "@/components/ProfileContacts";
+import TextScramble from '@/lib/TextScramble';
 
 const WhoAmI = () => {
+    const textRef = useRef(null);
+
+    useEffect(() => {
+        const phrases = [
+            "- ",
+            "-----",
+            "----------------",
+            "-----------------------------------------",
+            "-------------------------------------------------------------------",
+            "--------------------------------------------------------------------------------------"
+        ];
+
+        const fx = new TextScramble(textRef.current);
+
+        let counter = 0;
+        const next = () => {
+            fx.setText(phrases[counter]).then(() => {
+                setTimeout(next, 800);
+            });
+            counter = (counter + 1) % phrases.length;
+        };
+
+        next();
+    }, []);
+    
     return (
         <div className="flex flex-col items-center justify-center flex-1 h-[100%] pt-10 text-white">
             <div className="mt-20 flex flex-col items-center justify-center flex-1 h-[100%]">
@@ -16,6 +43,7 @@ const WhoAmI = () => {
 
                 <div className="font-sub_title text-slate-300 mt-1">
                     I'm a third-year college student currently pursuing my bachelor's degree in <strong className="text-white">Computer Science</strong>.
+                    <div className="text-center" ref={textRef}></div>
                 </div>
             </div>
         </div>
