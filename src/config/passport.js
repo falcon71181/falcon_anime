5978552d37a4f10b74d3ed3404589a1057681b5f
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-27 16:59:44 +0530

    Passport.js Added

---
+++ src/config/passport.js
@@ -0,0 +1,43 @@
+import { Strategy } from 'passport-local';
+import bcrypt from 'bcryptjs';
+import { User } from '../Models/User.js';
+
+const configurePassport = (passport) => {
+  passport.use(
+    new Strategy({ usernameField: 'email' }, async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: 'User Not Found' });
+        }
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Wrong Password' });
+          }
+        });
+      } catch (error) {
+        return done(error);
+      }
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  });
+};
+
+export default configurePassport;
