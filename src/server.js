3ef6312ae2267389e4e594810b40578fb9e375ec
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-28 09:33:27 +0530

    used Mongoose & Added CORS Config

--- src/server.js
+++ src/server.js
@@ -1,12 +1,66 @@
 import express from "express";
+import mongoose from 'mongoose';
+import passport from 'passport';
+import flash from 'connect-flash';
+import session from 'express-session';
 import router from "./Routes/router.js";
 import { URLs } from "./Utils/constantDATA.js";
 import cors from 'cors';
+import configurePassport from "./config/passport.js";
+import { mongoURL } from "./config/keys.js";
+import fetch from 'node-fetch';
 
 const app = express();
 const BACKEND_PORT = process.env.BACKEND_PORT || 3001;
 
-app.use(cors());
+mongoose
+  .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('⚔️ MongoDB Connected ⚔️'))
+  .catch(err => console.log(err));
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: process.env.SECRET || 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Initialize Passport and configure it
+configurePassport(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+const corsOptions = {
+  origin: process.env.CLIENT,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
+// Additional headers to handle redirection
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', process.env.CLIENT);
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
+
 
 async function isSiteReachable(url) {
   try {
@@ -17,17 +71,19 @@ async function isSiteReachable(url) {
   }
 }
 
-const isAniwatchUP = await isSiteReachable(URLs.BASE);
+async function handleHealthCheck(req, res) {
+  const isAniwatchUP = await isSiteReachable(URLs.BASE);
 
-app.get("/health", (req, res) => {
   res.status(200).json({
     API: true,
     aniwatch: true === isAniwatchUP
   });
-});
+}
+
+app.get("/health", handleHealthCheck);
 
 app.use("/", router);
 
 app.listen(BACKEND_PORT, () => {
-    console.log(`⚔️  API @ ON PORT : ${BACKEND_PORT} @ STARTED  ⚔️`);
+  console.log(`⚔️  API @ ON PORT : ${BACKEND_PORT} @ STARTED  ⚔️`);
 });
