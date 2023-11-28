b696726ea05dd1b0270c217348ff9c45c1a74866
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-28 16:32:01 +0530

    Token Validators Middlewares Added

--- src/Middleware/Export.js
+++ src/Middleware/Export.js
@@ -1,7 +1,10 @@
 import { validateCategory } from "./validateCategory.js";
 import { validateGenre } from "./validateGenre.js";
+import { ensureAuthenticated, validAuthentication } from './auth.js';
 
 export {
     validateCategory,
-    validateGenre
+    validateGenre,
+    ensureAuthenticated, 
+    validAuthentication
 };
--- src/Routes/router.js
+++ src/Routes/router.js
@@ -8,18 +8,33 @@ import {
 } from '../Controllers/Export.js';
 import {
     validateCategory,
-    validateGenre
+    validateGenre,
+    ensureAuthenticated, 
+    validAuthentication
 } from '../Middleware/Export.js';
 import { registration } from "../Utils/registerUSER.js";
 import { login } from "../Utils/loginUSER.js";
 
 const router = Router()
 
+function test(req, res){
+  const data = {
+    user:'',
+  }
+  res.status(200).json(data);
+}
+
+// /valid
+router.get("/valid", validAuthentication);
+
 // /api/register
 router.post("/api/register", registration);
 
 // /api/login
 router.post('/api/login', login);
+
+// /profile
+router.get('/profile', ensureAuthenticated, test);
 
 // /anime/search?keyword=${query}&page=${page}
 router.get("/anime/search", getSearch);
@@ -35,8 +50,5 @@ router.get("/:category", validateCategory, getCategory);
 
 // /genre/:genre?page=${page}
 router.get("/genre/:genre", validateGenre, getGenre)
-
-// /
-router.get("/", (_, res) => res.redirect("/"));
 
 export default router;
