5391e28c04c8391bbdab02fe08fe8dd387ec1190
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-29 10:01:47 +0530

    Token Validation Sate Added

--- src/client/app/login/page.jsx
+++ src/client/app/login/page.jsx
@@ -1,12 +1,36 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import LoginForm from "@/components/LoginForm";
+import { isSessionIDValid } from "@/lib/IsSessionIDValid";
+import { redirect } from 'next/navigation';
 
-const login = () => {
-    return(
-        <div className='min-h-screen pt-28 px-10'>
-            <h1 className='text-5xl font-bold'>Member</h1>
-            <LoginForm></LoginForm>
-        </div>
-    )
-}
+const Login = () => {
+  const [isDataFetched, setIsDataFetched] = useState(false);
 
-export default login;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await isSessionIDValid();
+        if (data.valid === true) {
+          redirect('/profile');
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsDataFetched(true);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className='min-h-screen pt-28 px-10'>
+      <h1 className='text-5xl font-bold'>Member</h1>
+      {isDataFetched && <LoginForm />}
+    </div>
+  );
+};
+
+export default Login;
