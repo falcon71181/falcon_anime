77c6e9a8f44c6f6e336351e67e734a765be95fe3
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-29 08:48:42 +0530

    Token Validation () Added

---
+++ src/client/lib/IsSessionIDValid.js
@@ -0,0 +1,26 @@
+const isSessionIDValid = async () => {
+    try {
+        console.log('isSessionIDValid Working')
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/valid`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          // Add any additional headers if needed
+        },
+        // Any other options...
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch user profile');
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching user profile:', error.message);
+      throw error;
+    }
+  };
+
+  export { isSessionIDValid };
