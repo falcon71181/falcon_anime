55578c90148036a40dafbbffabae34c92c8a10fe
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-01 15:27:06 +0530

    TopAiringAnimes Added

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -19,10 +19,13 @@ import "swiper/swiper-bundle.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Link from "next/link";
 import { getHomeData } from "@/lib/utils";
+import HomeAnimeCard from "@/components/HomeAnimeCard";
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 const Home = async () => {
   const homeData = await getHomeData();
+  const topAiringAnimes = homeData.topAiringAnimes;
+  console.log(topAiringAnimes);
 
   return (
     <div className="min-h-screen pt-24 px-10 text-left">
@@ -50,7 +53,9 @@ const Home = async () => {
               <div className="text-yellow-400 mb-10 font-bold font-ubuntu uppercase md:text-1 hidden md:inline md:text-xs">
                 <p className="text-4xl">#{anime.sno + 1} Spotlight</p>
               </div>
-              <h1 className="font-ubuntu pb-5 text-white tracking-wide mb-20 text-5xl md:text-3xl md:leading-tight md:line-clamp-2 lg:line-clamp-2 md:mb-5 md:text-2xl lg:text-2xl lg:mb-0 md:leading-5 lg:leading-5 md:tracking-tight md:mb-0 lg:mb-0">{anime.name}</h1>
+              <h1 className="font-ubuntu pb-5 text-white tracking-wide mb-20 text-5xl md:text-3xl md:leading-tight md:line-clamp-2 lg:line-clamp-2 md:mb-5 md:text-2xl lg:text-2xl lg:mb-0 md:leading-5 lg:leading-5 md:tracking-tight md:mb-0 lg:mb-0">
+                {anime.name}
+              </h1>
               <div className="flex text-sm pb-5 items-center md:gap-1 md:font-base md:items-center md:mb-0 md:flex md:text-base lg:flex lg:text-base lg:gap-1 lg:items-center lg:mb-0">
                 <p className="flex items-center gap-2 text-white">
                   <FaPlayCircle size={12} />
@@ -63,25 +68,40 @@ const Home = async () => {
                   <FaCalendar size={12} />
                   {anime.extra.releasedDay}
                 </p>
-                <span className="inline-block px-2 py-1 bg-yellow-400 text-black rounded font-semibold text-xs">{anime.extra.quality}</span>
+                <span className="inline-block px-2 py-1 bg-yellow-400 text-black rounded font-semibold text-xs">
+                  {anime.extra.quality}
+                </span>
               </div>
-              <div className="overflow-ellipsis pb-10 mr-10 font-poppins text-white text-base md:line-clamp-2 md:leading-tight md:my-0 md:text-sm lg:text-base lg:leading-tight lg:my-0 lg:text-sm xl:line-clamp-4 xl:leading-tight xl:my-0 xl:text-base">{anime.description}</div>
+              <div className="overflow-ellipsis pb-10 mr-10 font-poppins text-white text-base md:line-clamp-2 md:leading-tight md:my-0 md:text-sm lg:text-base lg:leading-tight lg:my-0 lg:text-sm xl:line-clamp-4 xl:leading-tight xl:my-0 xl:text-base">
+                {anime.description}
+              </div>
               <div className="flex mt-5 items-center gap-4 md:gap-1 md:flex-wrap">
-                <Link href={`${process.env.NEXT_PUBLIC_CLIENT}/anime/${anime.id}`} /// in future it need to UPDATE
-                className="flex items-center gap-1 p-2 bg-yellow-400 text-black text-lg rounded-full font-ubuntu">
+                <Link
+                  href={`${process.env.NEXT_PUBLIC_CLIENT}/anime/${anime.id}`} /// in future it need to UPDATE
+                  className="flex items-center gap-1 p-2 bg-yellow-400 text-black text-lg rounded-full font-ubuntu"
+                >
                   <FaPlayCircle />
                   Watch Now
                 </Link>
-                <Link href={`${process.env.NEXT_PUBLIC_CLIENT}/anime/${anime.id}`}
-                className="flex items-center p-2 bg-gray-700 text-white gap-1 text-lg rounded-full font-ubuntu">
+                <Link
+                  href={`${process.env.NEXT_PUBLIC_CLIENT}/anime/${anime.id}`}
+                  className="flex items-center p-2 bg-gray-700 text-white gap-1 text-lg rounded-full font-ubuntu"
+                >
                   Detail <FaChevronRight size={12} />
                 </Link>
               </div>
             </div>
           </SwiperSlide>
         ))}
-
       </Swiper>
+      <div className="min-h-screen pt-5 px-10">
+        <h1 className="text-5xl font-bold">Top Airing Animes</h1>
+        <div className="grid grid-cols-4 gap-8 gap-y-14 mt-12">
+          {topAiringAnimes.map((anime) => (
+            <HomeAnimeCard key={anime.id} anime={anime} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/components/HomeAnimeCard.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// TODO: Shift to env var
+const CLIENT_BASE_URL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://falcon-anime.vercel.app';
+
+const HomeAnimeCard = ({ anime }) => {
+    return (
+        <Link
+            key={anime.id}
+            href={`${CLIENT_BASE_URL}/anime/${anime.id}`}
+            className='group transition-colors rounded-lg bg-gradient-to-b from-stone-900 via-transparent to-stone-900 hover:bg-stone-700 hover:shadow-lg hover:shadow-grny transition-shadow'
+        >
+            <div className='rounded-lg overflow-hidden'>
+                <Image
+                    className="transition h-[350px] w-[450px] rounded-lg  group-hover:scale-125"
+                    src={anime.img}
+                    width={350}
+                    height={450}
+                    alt={`$${anime.name} Image`}
+                    objectFit="cover"
+                />
+            </div>
+            <div className='py-5 px-4'>
+                <h1 className='truncate text-stone-300 font-bold'>{anime.name}</h1>
+            </div>
+        </Link>
+    )
+}
+
+export default HomeAnimeCard;
